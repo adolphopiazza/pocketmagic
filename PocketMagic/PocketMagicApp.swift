647bc0f374aaf29249b4bcb39849e34cb8b8cb25
Adolpho Piazza <adolphopiazza@gmail.com>
2023-10-03 22:45:58 -0300

    Initial commit

---
+++ PocketMagic/PocketMagicApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketMagicApp.swift
+//  PocketMagic
+//
+//  Created by Adolpho Francisco Zimmermann Piazza on 03/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PocketMagicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
