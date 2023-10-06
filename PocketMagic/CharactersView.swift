c1a4483e66b652957ebc81f72b79efbd66cbe980
Adolpho Piazza <adolphopiazza@gmail.com>
2023-10-05 21:52:33 -0300

    Fetch from characters API. WORK IN PROGRESS, this is really basic. Needs a lot of refactoring.

--- PocketMagic/CharactersView.swift
+++ PocketMagic/CharactersView.swift
@@ -7,10 +7,64 @@
 
 import SwiftUI
 
+struct CharactersModel: Decodable, Identifiable {
+    let id: UUID
+    let name: String
+    let actor: String
+}
+
+class Networking {
+    
+    let baseAPI = "https://hp-api.onrender.com/api/characters"
+    
+    func fetchCharacters() async throws -> [CharactersModel] {
+        guard let url = URL(string: baseAPI) else { throw NSError() }
+        let model: [CharactersModel]
+        
+        do {
+            // talvez separar em uma função para cada try
+            let (data, _) = try await URLSession.shared.data(from: url)
+            model = try JSONDecoder().decode([CharactersModel].self, from: data)
+        } catch {
+            throw error
+        }
+        
+        return model
+    }
+    
+}
+
 struct CharactersView: View {
     
+    @State private var characters: [CharactersModel] = []
+    
     var body: some View {
-        Text("Characters list view!")
+        NavigationStack {
+            List {
+                ForEach(characters) { character in
+                    VStack(alignment: .leading) {
+                        Text(character.name)
+                            .font(.headline)
+                        
+                        Text(character.actor.isEmpty ? "Not found" : character.actor)
+                            .font(.subheadline)
+                    }
+                }
+            }
+            .navigationTitle("Characters")
+        }
+        .onAppear(perform: {
+            let networking = Networking()
+            Task {
+                do {
+                    let model = try await networking.fetchCharacters()
+                    characters = model
+                    print(characters)
+                } catch {
+                    print(error)
+                }
+            }
+        })
     }
     
 }
